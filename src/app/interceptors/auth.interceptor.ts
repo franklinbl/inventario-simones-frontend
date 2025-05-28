d2e3287da1dbbdfd288390dbf89b5eda73d29da2
franklin barco <franklin@flowrms.com>
2025-05-28 17:02:01 -0400

    Enhance auth interceptor: add error handling for invalid tokens, triggering logout and redirecting to login page, improving user session management.

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,14 +1,27 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
   const token = localStorage.getItem('token');
 
   if (token) {
     const cloned = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
     });
-    return next(cloned);
+    return next(cloned).pipe(
+      catchError(error => {
+        if (error.error?.message === 'Token invalido') {
+          authService.logout();
+          router.navigate(['/login']);
+        }
+        return throwError(() => error);
+      })
+    );
   }
 
   return next(req);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://severe-pam-franklinbl-05c66c24.koyeb.app/api'
+  apiUrl: 'http://localhost:3000/api'
 };
