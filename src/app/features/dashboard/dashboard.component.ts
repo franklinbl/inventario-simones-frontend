3dc0871dca9872296c7c4e4eae34c06ce1b161fc
franklin barco <franklin@flowrms.com>
2025-05-28 20:22:56 -0400

    Refactor dashboard component: update HTML to display dynamic data for active events, monthly events, and low stock products, enhancing the dashboard's functionality and user experience. Implement data fetching in TypeScript using DashboardService for improved data management.

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardService } from './services/dashboard.service';
+import { DashboardAttributes } from './models/dashboard.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,4 +10,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  private dashboardService = inject(DashboardService);
+  infoDashboard: DashboardAttributes = {
+    pendingRentals: 0,
+    monthlyEvents: 0,
+    lowStockProducts: 0
+  };
+
+  ngOnInit(): void {
+    this.dashboardService.getInfoDashboard().subscribe((data) => {
+      this.infoDashboard = data;
+    });
+  }
+}
---
+++ src/app/features/dashboard/models/dashboard.model.ts
@@ -0,0 +1,5 @@
+export interface DashboardAttributes {
+  pendingRentals: number;
+  monthlyEvents: number;
+  lowStockProducts: number;
+}
